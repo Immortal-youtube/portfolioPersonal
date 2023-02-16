46bc2892ce49a9dd3a9879eaf6bdf7dad88417bf
Immortal <84240738+Immortal-youtube@users.noreply.github.com>
2023-02-16 21:24:36 +0530

    run

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 //import { useState } from 'react';
 import { Helmet } from 'react-helmet';
-//const discord = require('./icons/discord.png')
-//const youtube = require('./icons/youtube.png')
-//const github = require('./icons/github.png')
-//const python = require('./icons/python.png')
-//const java = require('./icons/java.png')
-//const c = require('./icons/c.png')
-//const mongodb = require('./icons/mongodb.png')
-//const sql = require('./icons/sql.png')
+const discord = require('./icons/discord.png')
+const youtube = require('./icons/youtube.png')
+const github = require('./icons/github.png')
+const python = require('./icons/python.png')
+const java = require('./icons/java.png')
+const c = require('./icons/c.png')
+const mongodb = require('./icons/mongodb.png')
+const sql = require('./icons/sql.png')
 class Counter extends Component { 
 
     state={
@@ -25,10 +25,21 @@ class Counter extends Component {
                     <title>Immortal</title>
                 </Helmet>
                 
-                <h1 className='font-suk text-emerald-300 text-6xl text-center'>Hey there I'm Ansh</h1>
-                <p className= 'font-par text-center'>I am a full stack developer.I have made many minecraft plugins,Discord Bots, Arduino Projects,Websites,etc.
-                    <br/>My main languages are Python,Java and I have worked with MongoDB,SQL,etc.
+                <h1 className='font-suk text-emerald-300 text-6xl text-center'>Hey there, I'm Ansh!</h1>
+                <p className= 'font-par text-center py-3'>I am a full stack developer.I have made many minecraft plugins,Discord Bots, Arduino Projects,Websites,etc.
+                    <br/>My main languages are Python, Java and I have worked with MongoDB,SQL,etc.
                 </p>
+                <div className='text-center'>
+                    <a className = 'tooltip tooltip-bottom' data-tip='Join my server!' href='https://discord.gg/gqp5nMzwTY' target='_blank' rel="noreferrer"><img src={discord} alt='discord' className='w-20 h-20 m-3'/></a>
+                    <a className = 'tooltip tooltip-bottom' data-tip='Check out my channel!' href='https://www.youtube.com/channel/UCgppWM8JtpTfEYp8sa8XHEw' target='_blank' rel="noreferrer"><img src={youtube} alt='discord' className='w-20 h-20 m-3'/></a>
+                    <a className = 'tooltip tooltip-bottom' data-tip='See my projects!' href='https://github.com/Immortal-youtube' target='_blank' rel="noreferrer"><img src={github} alt='discord' className='w-20 h-20 m-3'/></a>
+                    <a className = 'tooltip tooltip-bottom' data-tip='Download Python!' href='https://www.python.org' target='_blank' rel="noreferrer"><img src={python} alt='discord' className='w-20 h-20 m-3'/></a>
+                    <a className = 'tooltip tooltip-bottom' data-tip='Download Java!' href='https://www.java.com/en/' target='_blank' rel="noreferrer"><img src={java} alt='discord' className='w-20 h-20 m-3'/></a>
+                    <a className = 'tooltip tooltip-bottom' data-tip='Checkout C++!' href='https://en.wikipedia.org/wiki/C%2B%2B' target='_blank' rel="noreferrer"><img src={c} alt='discord' className='w-20 h-20 m-3'/></a>
+                    <a className = 'tooltip tooltip-bottom' data-tip='Download MySQl!' href='https://www.mysql.com' target='_blank' rel="noreferrer"><img src={sql} alt='discord' className='w-20 h-20 m-3'/></a>
+                    <a className = 'tooltip tooltip-bottom' data-tip='Checkout MongoDB!' href='https://www.mongodb.com' target='_blank' rel="noreferrer"><img src={mongodb} alt='discord' className='w-20 h-20 m-3'/></a>
+                    
+                </div>
                 
             </div>
         );
