75deec752eefafd63695cb7b9658b6f7bde18c34
Immortal <84240738+Immortal-youtube@users.noreply.github.com>
2023-02-20 17:03:34 +0530

    send

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -14,13 +14,8 @@ const github = require('./icons/github.png')
 function Counter() { 
     let str=""
     console.log(window.innerHeight)
-    if(window.innerHeight < 820){
-        if(window.innerWidth < 700){
-            str = "bg-noob w-full h-full bg-cover bg-no-repeat"
-        }else{
-            str="bg-noob w-screen h-screen bg-cover"
-        }    
-        
+    if(window.innerHeight < 820){   
+        str="bg-noob w-full h-full bg-cover"
     }else{
         str="bg-noob w-screen h-screen bg-cover"
     }
