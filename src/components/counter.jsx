f51e5895758c2f297a0a50c01f3c5e212a1abc3c
Immortal <84240738+Immortal-youtube@users.noreply.github.com>
2023-03-18 11:13:56 +0530

    Sad

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,23 +5,23 @@ import Navbar from './navbar';
 import { Helmet } from 'react-helmet';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCode } from "@fortawesome/free-solid-svg-icons";
-const discord = require('./icons/discord.png')
-const youtube = require('./icons/youtube.png')
-const github = require('./icons/github.png')
-const css1 = require('./icons/csspics/css1.png')
-const css2 = require('./icons/csspics/css2.png')
-const css3 = require('./icons/csspics/css3.png')
-const css4 = require('./icons/csspics/css4.png')
-const css5 = require('./icons/csspics/css5.png')
-const css6 = require('./icons/csspics/css6.png')
-const css7 = require('./icons/csspics/css7.png')
-const css8 = require('./icons/csspics/css8.png')
-const css9 = require('./icons/csspics/css9.png')
-const css10 = require('./icons/csspics/css10.png')
-const css11 = require('./icons/csspics/css11.png')
-const css12 = require('./icons/csspics/css12.png')
-const css13 = require('./icons/csspics/css13.png')
-const css14 = require('./icons/csspics/css14.png')
+// const discord = require('./icons/discord.png')
+// const youtube = require('./icons/youtube.png')
+// const github = require('./icons/github.png')
+// const css1 = require('./icons/csspics/css1.png')
+// const css2 = require('./icons/csspics/css2.png')
+// const css3 = require('./icons/csspics/css3.png')
+// const css4 = require('./icons/csspics/css4.png')
+// const css5 = require('./icons/csspics/css5.png')
+// const css6 = require('./icons/csspics/css6.png')
+// const css7 = require('./icons/csspics/css7.png')
+// const css8 = require('./icons/csspics/css8.png')
+// const css9 = require('./icons/csspics/css9.png')
+// const css10 = require('./icons/csspics/css10.png')
+// const css11 = require('./icons/csspics/css11.png')
+// const css12 = require('./icons/csspics/css12.png')
+// const css13 = require('./icons/csspics/css13.png')
+// const css14 = require('./icons/csspics/css14.png')
 
 
 
@@ -51,8 +51,10 @@ export default function Counter() {
     // }
         
     if (x_html < x_screen){
+        console.log("shit")
         str="bg-noob  w-screen h-screen bg-cover bg-fixed"
     }else{
+        console.log("wtf")
         str="bg-noob w-full h-full bg-cover bg-fixed"
     }
     console.log(x_html)
@@ -70,11 +72,7 @@ export default function Counter() {
             <p className= 'font-par text-center text-white py-5'>I am a <p className='inline font-high text-purple-400'>full stack developer</p>.I have made many minecraft plugins, Discord Bots, Arduino Projects, Websites, etc.
                 <br/>My main languages are Python, Java and I have worked with MongoDB,SQL,etc.
             </p>
-            <div className='text-center my-9'>
-                <a className = 'tooltip tooltip-bottom' data-tip='Join my server!' href='https://discord.gg/gqp5nMzwTY' target='_blank' rel="noreferrer"><img src={discord} alt='discord' className='hover:scale-110 duration-300 rounded-full w-20 h-20 mx-4 hover:shadow-2xl hover:shadow-lime-300'/></a>
-                <a className = 'tooltip tooltip-bottom' data-tip='Check out my channel!' href='https://www.youtube.com/channel/UCgppWM8JtpTfEYp8sa8XHEw' target='_blank' rel="noreferrer"><img src={youtube} alt='discord' className='hover:scale-110 rounded-full duration-300 w-20 h-20 mx-4 hover:shadow-2xl hover:shadow-lime-300'/></a>
-                <a className = 'tooltip tooltip-bottom' data-tip='See my projects!' href='https://github.com/Immortal-youtube' target='_blank' rel="noreferrer"><img src={github} alt='discord' className='hover:scale-110 duration-300 w-20 h-20 mx-4 bg-white rounded-full hover:shadow-2xl hover:shadow-lime-300'/></a>
-            </div>
+            
             {/* <h2 className=' font-par text-white flex-auto px-5 '>On going Projects:
                 <br/> <FontAwesomeIcon icon={faCode} /> Computer Vision based Cube Solver(Kociemba's Algorithm)
                 <br/> <FontAwesomeIcon icon={faCode} /> Updating Discord bot  
@@ -91,30 +89,24 @@ export default function Counter() {
                 <li className="font-par text-white flex-auto px-5 py-1">
                 <FontAwesomeIcon icon={faCode} className="text-emerald-400"/> Voice Assistant using ChatGPT
                 </li>
+                <li className="font-par text-white flex-auto px-5 py-1">
+                <FontAwesomeIcon icon={faCode} className="text-emerald-400"/> Project CLI (To setup different kinds of Projects)
+                </li>
+                <li className="font-par text-white flex-auto px-5 py-1">
+                <FontAwesomeIcon icon={faCode} className="text-emerald-400"/> Personal Firewall
+                </li>
             </ul>
-            <h2 className=' text-purple-400 font-par flex-auto pt-5 px-5'>
-                Connect with me on Discord or mail me at: 
-            </h2>
-            <h2 className='text-purple-300 font-par flex-auto px-5 py-1'>
-                immortal.gaming0808@gmail.com
-                </h2>
-            <h2 className='font-high text-purple-400 flex-auto px-5 py-3'>Casual flexing:</h2>
-            <div className="pl-2">
-            {/* <img src={css1} alt="not loading"/>
-            <img src={css2} alt="not loading"/>
-            <img src={css3} alt="not loading"/>
-            <img src={css4} alt="not loading"/>
-            <img src={css5} alt="not loading"/>
-            <img src={css6} alt="not loading"/>
-            <img src={css7} alt="not loading"/>
-            <img src={css8} alt="not loading"/>
-            <img src={css9} alt="not loading"/>
-            <img src={css10} alt="not loading"/>
-            <img src={css11} alt="not loading"/>
-            <img src={css12} alt="not loading"/>
-            <img src={css13} alt="not loading"/>
-            <img src={css14} alt="not loading"/> */}
-            </div>
+            <h2 className="font-high text-purple-400 flex-auto px-5 py-2">Why do I code ?</h2>
+            <p className="font-par text-white flex-auto px-5 py-1">Coding or Programming is considered as just memorising syntax and using it to build Applications 
+            which is not true at all. Such stereotypes have forced many to switch gears and go away from code.</p>
+            
+
+            {/* <div className='flex justify-center space-x-4'>
+                <a className = 'tooltip tooltip-bottom ' data-tip='Join my server!' href='https://discord.gg/gqp5nMzwTY' target='_blank' rel="noreferrer"><img src={discord} alt='discord' className='hover:scale-110 duration-300 rounded-full w-20 h-20 mx-4 hover:shadow-2xl hover:shadow-lime-300'/></a>
+                <a className = 'tooltip tooltip-bottom ' data-tip='Check out my channel!' href='https://www.youtube.com/channel/UCgppWM8JtpTfEYp8sa8XHEw' target='_blank' rel="noreferrer"><img src={youtube} alt='discord' className='hover:scale-110 rounded-full duration-300 w-20 h-20 mx-4 hover:shadow-2xl hover:shadow-lime-300'/></a> 
+                <a className = 'tooltip tooltip-bottom ' data-tip='See my projects!' href='https://github.com/Immortal-youtube' target='_blank' rel="noreferrer"><img src={github} alt='discord' className='hover:scale-110 duration-300 w-20 h-20 mx-4 bg-white rounded-full hover:shadow-2xl hover:shadow-lime-300'/></a>
+            </div> */}
+
         </div>
         
     );
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,17 @@
 import {Link} from "react-router-dom"
+
+
 export default function Navbar(){
     return(
-        <nav className='navbar text-purple-400 font-high flex-auto bg-black sticky top-0'>
+        <nav className='navbar text-purple-400 font-high flex bg-black sticky top-0'>
             <ul>
                 <li>
-                    <Link className="mx-4 px-4 py-4 rounded-xl hover:bg-emerald-400 duration-300 hover:text-black" to="/">Home</Link>
+                    <Link className="mx-4 text-center px-4 py-4 rounded-xl hover:bg-emerald-400 duration-300 hover:text-black" to="/">Home</Link>
                 </li >
                 <li>
-                    <Link className="mx-4 px-4 py-4 rounded-xl hover:bg-emerald-400 duration-300 hover:text-black " to="/about">About me</Link>
+                    <Link className="mx-4 text-center px-4 py-4 rounded-xl hover:bg-emerald-400 duration-300 hover:text-black " to="/about">About me</Link>
                 </li>
+                
             </ul>
         </nav>
     );
