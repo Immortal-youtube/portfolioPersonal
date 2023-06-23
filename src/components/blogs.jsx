7b65318f6d6b893d106aab171dc7085e09dac46a
Immortal <84240738+Immortal-youtube@users.noreply.github.com>
2023-06-23 15:44:00 +0530

    new blog 2

--- src/components/blogs.jsx
+++ src/components/blogs.jsx
@@ -50,19 +50,30 @@ export default function Blogs(){
             <Helmet>
                 <title>Blogs</title>
             </Helmet>
-            <h2 className="font-suk text-purple-300 text-6xl text-center pb-14">Blogs</h2>
-            <div className="border-purple-400 border-4 mx-8 w-auto h-fit bg-black duration-300 mb-5">
-                    <h2 className="font-high text-purple-300 px-5 pt-3">Perfection in the Tech Industry</h2>
-                    <h2 className="font-high text-right text-white pt-3 px-5">21/06/2023</h2>
-                    <p className="font-par text-white px-5 py-5">In the text industry, perfection is essential.The number of people who are familiar 
-                    with Steve Jobs is one example. There is a chance that if you ask the same folks who Andy Rubin is, they won't know. As a community of 
-                    people who use and appreciate technology, we all tend to be so preoccupied with the greatest that we never consider the runners up. 
-                    Human nature is what it is, and it cannot be changed. Because all you want is the newest iPhone, why should you be concerned about 
-                    Andy Rubin, the man who built Android. Because Apple produces some of the best goods, we all covet Apple stuff. Although they are 
-                    pricey, the idea that Apple is the finest has already been ingrained in our minds.The key takeaway is that only the person who placed first is ever remembered. </p>
+                <h2 className="font-suk text-purple-300 text-6xl text-center pb-14">Blogs</h2>
+                <div className="border-purple-400 border-4 mx-8 w-auto h-fit bg-black duration-300 mb-5">
+                        <h2 className="font-high text-purple-300 px-5 pt-3">Rapid changes</h2>
+                        <h2 className="font-high text-right text-white pt-3 px-5">23/06/2023</h2>
+                        <p className="font-par text-white px-5 py-5">We all don't need to be reminded that technology advances quickly, just a blink and a new kind of chipset or processor
+                        might come out. One example could be how before you would have a 24 pin connector for your motherboard, well now that is replaced by 
+                        2 x 6 pin connectors. These rapid changes can also bee seen in prices. We all remember the good old days when buying a graphic cards did not make an 
+                        irrepairable hole in your wallet. Before the rule was that the more money you are willing to spend, the more performance you get. Well we now
+                        see that rule being broken as you no longer get the same kind of increment in performance no matter how much money you are willing to throw in. </p>
                 </div>
-            </div>
+                <div className="border-purple-400 border-4 mx-8 w-auto h-fit bg-black duration-300 mb-5">
+                        <h2 className="font-high text-purple-300 px-5 pt-3">Perfection in the Tech Industry</h2>
+                        <h2 className="font-high text-right text-white pt-3 px-5">21/06/2023</h2>
+                        <p className="font-par text-white px-5 py-5">In the text industry, perfection is essential.The number of people who are familiar 
+                        with Steve Jobs is one example. There is a chance that if you ask the same folks who Andy Rubin is, they won't know. As a community of 
+                        people who use and appreciate technology, we all tend to be so preoccupied with the greatest that we never consider the runners up. 
+                        Human nature is what it is, and it cannot be changed. Because all you want is the newest iPhone, why should you be concerned about 
+                        Andy Rubin, the man who built Android. Because Apple produces some of the best goods, we all covet Apple stuff. Although they are 
+                        pricey, the idea that Apple is the finest has already been ingrained in our minds.The key takeaway is that only the person who placed first is ever remembered. </p>
+                </div>
             
+                
+                
+            </div>
         </div>
     );
 }
